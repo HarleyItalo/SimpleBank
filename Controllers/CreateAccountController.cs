df65a2c9933103dabc1a7ca76415f51172ded091
Harley Italo Soares Xavier <harleyitalo@hotmail.com>
2023-08-30 13:33:36 -0300

    Creating project

---
+++ Controllers/CreateAccountController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBank.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CreateAccountController : ControllerBase
+{
+    public CreateAccountController()
+    {
+      
+    }
+}
